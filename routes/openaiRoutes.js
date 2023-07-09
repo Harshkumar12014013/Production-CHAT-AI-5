c9d2c2819124de8172b041e68b69e1e0c957b4cf
Harshkumar12014013 <101981203+Harshkumar12014013@users.noreply.github.com>
2023-07-09 19:20:16 +0530

    final code
    Chat AI deployment

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+
+const { summaryController, paragraphController, chatbotController, jsconverterController, imageController } = require("../controllers/openaiController");
+
+
+const router = express.Router();
+
+router.post('/summary', summaryController)
+router.post('/paragraph', paragraphController)
+router.post('/chatbot', chatbotController)
+router.post('/js-converter', jsconverterController)
+router.post('/image', imageController)
+
+module.exports = router;
